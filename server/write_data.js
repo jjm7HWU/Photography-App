6be9765ae51eca288bbb2953b6e711ceae8d575c
jjm7HWU <jjm7@hw.ac.uk>
2021-01-04 02:01:05 +0000

    s3 bucket file upload

---
+++ server/write_data.js
@@ -0,0 +1,21 @@
+const fs = require("fs");
+const s3 = require("../objects/bucket");
+
+function uploadImage(imageName) {
+
+  const contents = fs.readFileSync(imageName);
+
+  const params = {
+    Bucket: "photography-app-content",
+    Key: imageName,
+    Body: contents
+  };
+
+  s3.upload(params, function(err, data) {
+    if (err) throw err;
+    console.log("Image uploaded");
+  });
+
+}
+
+module.exports = uploadImage;

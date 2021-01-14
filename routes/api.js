c3c2d9a2b34d2198477cb7003abd91fbae080d35
jjm7HWU <jjm7@hw.ac.uk>
2021-01-14 16:30:24 +0000

    dummy APIs

--- routes/api.js
+++ routes/api.js
@@ -9,6 +9,22 @@ const { database } = require("../key");
 
 const router = express.Router();
 
+/* DUMMY FUNCTION - To be implemented */
+/* This will retrieve the user data from the database */
+function getUser(username) {
+  return {
+    username: username,
+    country: "United Kingdom",
+    area: "Edinburgh",
+    joindate: "27 Mar 2021",
+    bio: "I mostly do city life stuff but occasionally slip out into the wilderness.",
+    rank: 12,
+    points: 132,
+    followers: 137,
+    following: 43
+  }
+}
+
 /* Sends information for photo of specified reference number */
 router.get("/photo/:ref", (req, res) => {
 
@@ -19,6 +35,82 @@ router.get("/photo/:ref", (req, res) => {
   collection.findOne({ "ref": req.params.ref }).then(entry => {
     res.send(entry);
   });
+
+});
+
+/* Sends dummy user information */
+router.get("/user/:username", (req, res) => {
+
+  let user = getUser(req.params.username);
+
+  res.send(user);
+
+});
+
+/* Sends dummy leaderboard information as an array with elements of the following format */
+/*
+    username: string
+    position: number
+    rank: number
+    points: number
+    country: string
+*/
+router.get("/leaderboard/global", (req, res) => {
+
+  let leaderboardNames = [
+    "Eva", "Hans", "Maria", "Leah", "Felipe",
+    "Yusuf", "Luis", "Claire", "Mia", "Siddhartha",
+    "Lucas", "Mikaila", "Roseanna", "Jean", "Lee"
+  ];
+
+  let leaderboard = new Array();
+
+  leaderboardNames.forEach((username, position) => {
+    let user = getUser(username);
+    let entry = {
+      username: username,
+      position: position,
+      rank: 100,
+      points: 2325 + Math.pow(5-position, 3),
+      country: user.country
+    }
+    leaderboard.push(entry);
+  });
+
+  res.send(leaderboard);
+
+});
+
+/* Sends dummy feed for specified user. Each element is a photo. */
+router.get("/feed/:username", (req, res) => {
+  let posts = [
+    {
+      poster: "Felipe",
+      ref: 1,
+      hearts: 137,
+      comments: 19,
+      location: "Mongolia",
+      caption: "A cute marmot"
+    },
+    {
+      poster: "Hannah",
+      ref: 2,
+      hearts: 121,
+      comments: 6,
+      location: "Shetland, United Kingdom",
+      caption: "This is a puffin that I found"
+    },
+    {
+      poster: "Michael",
+      ref: 3,
+      hearts: 73,
+      comments: 4,
+      location: "Australia",
+      caption: "Koala bear"
+    }
+  ];
+
+  res.send(posts);
 
 });
 

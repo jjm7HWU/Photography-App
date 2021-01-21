55a0e13750d31b3ab48348b803a5d661b79ed70c
jjm7HWU <jjm7@hw.ac.uk>
2021-01-21 20:32:56 +0000

    remove repeated routes

--- routes/view.js
+++ routes/view.js
@@ -5,34 +5,6 @@ const path = require("path");
 
 const router = express.Router();
 
-/* Serves page containing post according to reference number */
-router.get("/photo/:ref", (req, res) => {
-
-  res.sendFile(path.join(__dirname, "..", "views", "photo.html"));
-
-});
-
-/* Serves page containing user profile */
-router.get("/profile/:username", (req, res) => {
-
-  res.sendFile(path.join(__dirname, "..", "views", "profile.html"));
-
-});
-
-/* Serves page containing post according to reference number */
-router.get("/leaderboard/:region", (req, res) => {
-
-  res.sendFile(path.join(__dirname, "..", "views", "leaderboard.html"));
-
-});
-
-/* Serves registration page */
-router.get("/register", (req, res) => {
-
-  res.sendFile(path.join((__dirname), "..", "views", "register.html"));
-
-});
-
 router.get("/:page/:x", (req, res) => {
 
   res.sendFile(path.join(__dirname, "..", "views", `${req.params.page}.html`));

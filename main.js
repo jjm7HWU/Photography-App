0782d799c60c15b3cec6e55c18d30968a77aabb2
jjm7HWU <jjm7@hw.ac.uk>
2021-03-29 22:24:16 +0100

    reference env port

--- main.js
+++ main.js
@@ -35,5 +35,5 @@ app.use("/api", require("./routes/api"));
 app.use("/", require("./routes/view"));
 
 // listen on port
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => console.log("Listening on port " + PORT));
